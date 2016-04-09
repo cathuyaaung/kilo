7b0f0b2c52021951da1260485a18f0bcca31d1ec
tinhtutnaing <tinhtutnaing@gmail.com>
2016-04-09 19:31:26 +0800

    -

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('kilo', ['ionic', 'kilo.controllers', 'kilo.services'])
+angular.module('kilo', ['ionic','ngCordova', 'kilo.controllers', 'kilo.services'])
 
 .run(
   ['$ionicPlatform','$rootScope','$state','AppValue',  
--- www/js/controller/dashDetailsPostCtrl.js
+++ www/js/controller/dashDetailsPostCtrl.js
@@ -4,24 +4,54 @@
         
          app.controller('DashDetailsPostCtrl', DashDetailsPostCtrl);
          
-         DashDetailsPostCtrl.$inject = ['$rootScope','$scope','$state','$stateParams','AppValue','postService','routeService'];
+         DashDetailsPostCtrl.$inject = ['$ionicPlatform','$rootScope','$scope','$state','$stateParams','$cordovaDatePicker','AppValue','postService','routeService'];
          
-         function DashDetailsPostCtrl($rootScope,$scope,$state,$stateParams,AppValue,postService,routeService){
+         function DashDetailsPostCtrl($ionicPlatform,$rootScope,$scope,$state,$stateParams,$cordovaDatePicker,AppValue,postService,routeService){
              
              var _sc = $scope.data = {                
                postId : $stateParams.postId                                      
             }
+            
+            
              
             $scope.$on('$ionicView.enter', function(e) {                
                 console.log(_sc.postId);
                 AppValue.tabBrowse = "tab.dashdetailspost";                                
             });
     
-           console.log($state);
+            console.log($state);
                 
-        function failed(params) {
+            function failed(params) {
                 
-            }                        
+            }
+            
+            $ionicPlatform.ready(function() {
+                
+                  var options = {
+                        date: new Date(),
+                        mode: 'date', // or 'time'
+                        minDate: new Date() - 10000,
+                        allowOldDates: true,
+                        allowFutureDates: false,
+                        doneButtonLabel: 'DONE',
+                        doneButtonColor: '#F2F3F4',
+                        cancelButtonLabel: 'CANCEL',
+                        cancelButtonColor: '#000000'
+                    };
+                
+                $scope.openDatePicker = function(){
+                  
+                    alert('Calender');
+                    
+                    console.log($cordovaDatePicker);
+                    
+                    $cordovaDatePicker.show(options).then(function(date){
+                        alert(date);
+                    });
+                }        
+            });
+            
+                           
             
             
          }
